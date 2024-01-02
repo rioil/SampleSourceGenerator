1592345a5285a95d17159bbefdb4626aa6f02c75
rioil <rioil.dev@gmail.com>
2024-01-02 17:41:44 +0900

    プロパティ生成を実装

---
+++ SampleSourceGenerator/MyModelObject.cs
@@ -0,0 +1,27 @@
+﻿using SourceGenerator;
+using System.ComponentModel;
+
+namespace ConsoleApp
+{
+    internal partial class MyModelObject : INotifyPropertyChanged
+    {
+        [NotificationProperty]
+        private int _intField1, _intField2, _intField3;
+
+        [NotificationProperty]
+        private long _longField;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+
+    internal partial class InvalidMyModelObject
+    {
+        [NotificationProperty]
+        private int _intField1, _intField2, _intField3;
+
+        [NotificationProperty]
+        private long _longField;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
--- SampleSourceGenerator/Program.cs
+++ SampleSourceGenerator/Program.cs
@@ -6,6 +6,15 @@
         {
             HelloFrom("Generated Code");
             Console.WriteLine(ConstStrings.Message);
+
+            var model = new MyModelObject
+            {
+                IntField1 = 1,
+                IntField2 = 2,
+                IntField3 = 3,
+                LongField = 4,
+            };
+            Console.WriteLine($"{model.IntField1}, {model.IntField2}, {model.IntField3}, {model.LongField}");
         }
 
         static partial void HelloFrom(string name);
